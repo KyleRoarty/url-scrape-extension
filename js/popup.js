80e8ae3d3ffe86758f00f162c09a4e58debeb557
Kyle Roarty <kroarty@xes-inc.com>
2017-11-07 15:25:17 -0600

    Allow remote-origin stuff
    Signed-off-by: Kyle Roarty <kroarty@xes-inc.com>

--- js/popup.js
+++ js/popup.js
@@ -1,7 +1,19 @@
+function getURLs(url) {
+    var request = new XMLHttpRequest();
+    request.open("GET", url, true);
+    request.send(null);
+    request.onreadystatechange = function() {
+        if (request.readyState == 4)
+            //console.log(request.response);
+            console.log(request.response.getElementsByTagName("a"));
+    };
+}
+
 document.addEventListener('DOMContentLoaded', () => {
     var url = document.getElementById('in_url');
     console.log("Hello!");
     url.addEventListener('change', () => {
         console.log("New string! "+url.value);
+        getURLs(url.value);
     });
 });
