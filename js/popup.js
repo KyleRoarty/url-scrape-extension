63c6986432136dce2669ceaf3c34c10f497b1cfd
Kyle Roarty <kroarty@xes-inc.com>
2017-11-08 13:13:44 -0600

    Can now parse tags
    Signed-off-by: Kyle Roarty <kroarty@xes-inc.com>

--- js/popup.js
+++ js/popup.js
@@ -1,19 +1,29 @@
-function getURLs(url) {
-    var request = new XMLHttpRequest();
-    request.open("GET", url, true);
-    request.send(null);
-    request.onreadystatechange = function() {
-        if (request.readyState == 4)
-            //console.log(request.response);
-            console.log(request.response.getElementsByTagName("a"));
-    };
+function htmlParse(result) {
+    console.log(result);
+    console.log(typeof result);
+    console.log(result.getElementsByTagName("a"));
+    alert("Hello world. Kevin was right");
 }
+
+function getURLs(url, callback) {
+    var xhttp;
+    xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if ((xhttp.readyState == 4) && (xhttp.status == 200)) {
+            callback(xhttp.response);
+        }
+    };
+    xhttp.open("GET", url, true);
+    xhttp.responseType = "document";
+    xhttp.send(null);
+}
+
 
 document.addEventListener('DOMContentLoaded', () => {
     var url = document.getElementById('in_url');
     console.log("Hello!");
     url.addEventListener('change', () => {
         console.log("New string! "+url.value);
-        getURLs(url.value);
+        getURLs(url.value, htmlParse);
     });
 });
